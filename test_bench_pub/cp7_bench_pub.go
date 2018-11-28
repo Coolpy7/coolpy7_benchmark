c21d289e88dd0ed62de54fc734bd4e8415cc88e3
黎东海 <jacle169@21cn.com>
2018-11-28 17:36:54 +0800

    cp7 bench pub

--- test_bench_pub/cp7_bench_pub.go
+++ test_bench_pub/cp7_bench_pub.go
@@ -17,9 +17,13 @@ import (
 var urlString = flag.String("url", "tcp://username:password@192.168.100.2:1883", "broker url")
 var topic = flag.String("topic", "cp7sub%i", "pub topic")
 var workers = flag.Int("workers", 200, "number of workers")
+var interval = flag.Int("i", 10, "interval of connecting to the broker(ms)")
+var interval_of_msg = flag.Int("I", 1000, "interval of publishing message(ms)")
+var size = flag.Int("s", 256, "payload size")
 var cs = flag.String("cid", "client", "client id start with")
 var qos = flag.Uint("qos", 0, "pub qos level")
 var clearsession = flag.Bool("clear", true, "clear session")
+var pingtime = flag.String("keepalive", "300s", "keepalive")
 
 func main() {
 	flag.Parse()
@@ -33,7 +37,7 @@ func main() {
 		cf, err := cl.Connect(&client.Config{
 			BrokerURL:    *urlString,
 			CleanSession: *clearsession,
-			KeepAlive:    "180s",
+			KeepAlive:    *pingtime,
 			ValidateSubs: true,
 			ClientID:     *cs + id,
 		})
@@ -47,18 +51,21 @@ func main() {
 		}
 
 		clients[*cs+id] = cl
+		time.Sleep(time.Duration(*interval) * time.Millisecond)
 	}
 
+	bigData := make([]byte, *size, *size)
+
 	c := cron.New()
-	c.AddFunc("*/10 * * * * ?", func() {
+	c.AddFunc("*/"+strconv.Itoa(*interval_of_msg)+" * * * * ?", func() {
 		go func() {
 			for i := 0; i < *workers; i++ {
 				id := strconv.Itoa(i)
 				for _, v := range clients {
-					v.Publish(strings.Replace(*topic, "%i", id, 1), []byte("test"), uint8(*qos), false)
-					time.Sleep(5 * time.Millisecond)
+					v.Publish(strings.Replace(*topic, "%i", id, 1), bigData, uint8(*qos), false)
+					time.Sleep(time.Millisecond)
 				}
-				time.Sleep(5 * time.Millisecond)
+				time.Sleep(time.Millisecond)
 			}
 		}()
 	})
